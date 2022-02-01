90186bca279e939ca45bb1a23861f69db9f2fe8e
hmdhszd <hmdhszd@gmail.com>
2022-02-01 14:17:49 +0330

    added 1- Check connection to MongoDB.go

---
+++ 1- Check connection to MongoDB.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Person struct {
+	Name string
+	Age  int
+	City string
+}
+
+func main() {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+}
