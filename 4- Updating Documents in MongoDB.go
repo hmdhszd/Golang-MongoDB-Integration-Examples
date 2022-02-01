388d7d220a3aaf721d07d21a72a6522014a3410e
hmdhszd <hmdhszd@gmail.com>
2022-02-01 17:19:40 +0330

    added 4- Updating Documents in MongoDB.go

---
+++ 4- Updating Documents in MongoDB.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Trainer struct {
+	Name string
+	Age  int
+	City string
+}
+
+func main() {
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//------------------------------------------------------------------------------------
+
+	collection := client.Database("mydb").Collection("persons")
+
+	filter := bson.D{{"name", "hamid"}}
+
+	// $inc   -->   increment
+
+	update := bson.D{
+		{"$inc", bson.D{
+			{"age", 1},
+		}},
+	}
+
+	updateResult, err := collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Matched %v documents and updated %v documents.\n", updateResult.MatchedCount, updateResult.ModifiedCount)
+
+}
