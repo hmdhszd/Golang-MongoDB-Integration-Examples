4b305705aaa2a32b4a7d933f574da12307f772e0
hmdhszd <hmdhszd@gmail.com>
2022-02-01 17:19:04 +0330

    modified:   1- Check connection to MongoDB.go

--- 1- Check connection to MongoDB.go
+++ 1- Check connection to MongoDB.go
@@ -16,12 +16,15 @@ type Person struct {
 }
 
 func main() {
+
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	//--------------------------------------------------------------------------
 
 	err = client.Ping(context.TODO(), nil)
 
