1344d9c11d2409634c419e8e835a456513fa2313
hmdhszd <hmdhszd@gmail.com>
2022-02-01 17:21:06 +0330

    added 7- Delete One Document in MongoDB.go

---
+++ 7- Delete One Document in MongoDB.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Trainer struct {
+	Name string
+	Age  int
+	City string
+}
+
+func main() {
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//------------------------------------------------------------------------------------
+
+	collection := client.Database("mydb").Collection("persons")
+
+	filter := bson.D{{"name", "hamid"}}
+
+	deleteResult, err := collection.DeleteOne(context.TODO(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Deleted %v documents in the trainers collection\n", deleteResult.DeletedCount)
+
+}
